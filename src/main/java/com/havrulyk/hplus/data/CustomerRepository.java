d069fb80dd4875a0589eda7635e61f3fdc6d4ad1
Illia Gavryliuk <illia_gavryliuk@epam.com>
2023-06-04 18:52:04 +0300

    Finished GraphQL Course

---
+++ src/main/java/com/havrulyk/hplus/data/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.havrulyk.hplus.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Customer findByEmail(String email);
+}
